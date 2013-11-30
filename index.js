1aadafddfee9f0eec871ea10c65bc77299d66c80
Robert Sköld <robert@publicclass.se>
2013-11-30 18:43:15 +0100

    Removed comment from index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-/*
-{id: 1} -> 'id = 1'
-[{id: 1}, {type: 'photo'}] -> 'id = 1 OR type = "photo"'
-{id: 1, type: 'photo'} -> 'id = 1 AND type = "photo"'
-{type:['photo','video']} -> 'type IN ("photo","video")'
-{type:{not:'text'}} -> 'type != "text"'
-{type:{not:['text','video']}} -> 'type NOT IN ("text","video")'
-{type:{like:'te%'}} -> 'type LIKE "te%"'
-{type:{like:['te%','x']}} -> throw SyntaxError('like does not support []')
-*/
 module.exports = where;
 
 function where(query,options){
